05b0a632cb97aded45300440adc757c12758e3b0
rudiAiwe <rudi.aiwe@gmail.com>
2023-12-21 20:53:25 +0300

    add toast to ext

--- src/main/java/org/rudi/core/misc/exts/FragmentExt.kt
+++ src/main/java/org/rudi/core/misc/exts/FragmentExt.kt
@@ -1,5 +1,7 @@
 package org.rudi.core.misc.exts
 
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -14,4 +16,25 @@ fun Fragment.repeatWhenStarted(action: (suspend () -> Unit)) {
             action.invoke()
         }
     }
+}
+
+fun Fragment.showToast(
+    @StringRes
+    stringId: Int
+){
+    Toast.makeText(
+        this.requireContext(),
+        stringId,
+        Toast.LENGTH_LONG
+    ).show()
+}
+
+fun Fragment.showToast(
+    string: String
+){
+    Toast.makeText(
+        this.requireContext(),
+        string,
+        Toast.LENGTH_LONG
+    ).show()
 }
